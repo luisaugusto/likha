c55f1349653fe4283a95681a6dcaf0d7c36b9318
Luis Augusto <hello@luis.app>
2024-03-03 18:50:52 -0800

    update home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,32 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      We sell custom creations. Call us at 916-400-0821
-    </div>
+    <section style={{ background: "rgb(210, 194, 176)" }}>
+      <h1>LIKHA By Alna & Marga • customized creations</h1>
+      <Image
+        src={"/logo.png"}
+        alt="logo"
+        width={500}
+        height={500}
+        style={{ display: "block", padding: "0 40px" }}
+      />
+      <Link
+        href={"tel:9164000821"}
+        style={{
+          fontSize: "16px",
+          padding: "16px 32px",
+          borderRadius: "4px",
+          textTransform: "uppercase",
+          background: "#111",
+          color: "white",
+          fontWeight: 600,
+          textDecoration: "none",
+        }}
+      >
+        Message us to order
+      </Link>
+    </section>
   );
 }
